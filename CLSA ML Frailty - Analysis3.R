e8c44adab45e856a7c43d72e96950423a2857518
ZvanAllen <vanallen.22@gmail.com>
2024-06-28 12:55:50 -0400

    Update CLSA ML Frailty - Analysis.R

--- CLSA ML Frailty - Analysis3.R
+++ CLSA ML Frailty - Analysis3.R
@@ -4,7 +4,7 @@ library(tidyverse)  # general
 library(broom)      # preparing correlation tables
 library(discrim)    # niave bayes engine not included in tidymodels
 library(ranger)     # RF package
-library(klaR)       # Naive Bayes package [Masks dplyr::select] ugh!
+library(klaR)       # Naive Bayes package [Masks dplyr::select] 
 library(kknn)       # KNN package
 library(xgboost)    # XGBoost package
 library(nnet)       # Neural network package
@@ -17,61 +17,16 @@ cl <- makePSOCKcluster(all_cores)
 registerDoParallel(cl)
 
 # load data #### 
+# change to "ADL_DCLS_F2 %in% c(1),1," for sensitivity analysis
 CLSA.baseline.FU2<- read.csv("CLSA.baseline.followup2.csv") %>%
-  mutate(Binary_IADL.ADL_F2 = if_else(ADL_DCLS_F2 %in% c(1, 2), 1, 
+  mutate(Binary_IADL.ADL_F2 = if_else(ADL_DCLS_F2 %in% c(1,2), 1, 
                                       if_else(is.na(ADL_DCLS_F2), NA_integer_, 0)))
 
 # list of numeric variable
-numeric_vars <- c("PA2_DSCR2_MCQ","NUT_FBR_NB_COM","NUT_BRD_NB_COM","NUT_MEAT_NB_COM",
-                  "NUT_MTOT_NB_COM","NUT_CHCK_NB_COM","NUT_FISH_NB_COM","NUT_SASG_NB_COM","NUT_PATE_NB_COM",
-                  "NUT_SAUC_NB_COM","NUT_O3EG_NB_COM","NUT_EGGS_NB_COM","NUT_LEGM_NB_COM","NUT_NUTS_NB_COM",
-                  "NUT_FRUT_NB_COM","NUT_GREEN_NB_COM","NUT_PTTO_NB_COM","NUT_FRIE_NB_COM","NUT_CRRT_NB_COM",
-                  "NUT_VGOT_NB_COM","NUT_LWCS_NB_COM","NUT_CHSE_NB_COM","NUT_LWYG_NB_COM","NUT_YOGR_NB_COM",
-                  "NUT_CALC_NB_COM","NUT_DAIR_NB_COM","NUT_SALT_NB_COM","NUT_DSRT_NB_COM","NUT_CHOC_NB_COM",
-                  "NUT_BTTR_NB_COM","NUT_DRSG_NB_COM","NUT_CAJC_NB_COM","NUT_PURE_NB_COM","NUT_CAML_NB_COM",
-                  "NUT_WHML_NB_COM","NUT_LFML_NB_COM","NUT_CADR_NB_COM","SLE_HOUR_NB_COM","SLE_30DUR_NB_COM",
-                  "SLE_MIDDUR_NB_COM","SLE_STAYDUR_NB_COM","SLE_LGDUR_NB_COM","MEDI_NO_COM",
-                  "STP_DOTTIME_SS_COM","STP_WORTIME_SS_COM","STP_COLTIME_SS_COM","FAS_F_SCORE_COM",
-                  "FAS_A_SCORE_COM","FAS_S_SCORE_COM","CRT_MRT_CORRANS_COM","K10_DSCORE_MCQ",
-                  "PER_DSCR_EXT_MCQ","PER_DSCR_AGR_MCQ","PER_DSCR_CON_MCQ","PER_DSCR_EMOS_MCQ",
-                  "PER_DSCR_OPEX_MCQ","FAS_TOTAL_SCORE_COM","ADM_DCS_AGE_COM","WLK_TIME_COM",
-                  "BP_SYSTOLIC_ALL_AVG_COM","BP_DIASTOLIC_ALL_AVG_COM","BP_PULSE_ALL_AVG_COM",
-                  "CR_TIME_COM","CR_AVG_TIME_COM","GS_EXAM_MAX_COM","GS_EXAM_AVG_COM","HGT_HEIGHT_M_COM",
-                  "WGT_WEIGHT_KG_COM","HBP_AGE_NB_COM","HYP_UTHYRAGE_NB_COM","IHD_ANGIAGE_NB_COM",
-                  "STR_CVAAGE_NB_COM","DIA_AGE_NB_COM","CAO_COPDAGE_NB_COM","BAL_TIME_R_COM","BAL_TIME_L_COM",
-                  "BAL_BEST_COM","TON_QUALITYINDEX_R_COM","TON_QUALITYINDEX_L_COM","VA_ETDRS_BOTH_RSLT_COM",
-                  "WHC_WAIST_CM_COM","WHC_HIP_CM_COM","FRAX_OSTFX_COM","FRAX_OSTFX_BMD_COM","SDC_POPDENSITY_COM",
-                  "RET_SPSEAG_NB_TRM","AGE_NMBR","SMK_FRSTCG_AG","SMK_CGDL_AG","WHO_MPAG_AG","WHO_HRTAG_AG",
-                  "WHO_HRTYR_YR","RET_AGE_NB","LFP_LAST_NB","SSA_DPALL","DEP_CESD10","ADL_DSUM","SLS_DSCR",
-                  "SMK_DYCS","COG_REYI_SCORE","COG_AFT_SCORE_1","COG_AFT_SCORE_2","COG_MAT_SCORE",
-                  "COG_REYII_SCORE","SO2OMIYY","PM25DALYY_01","PM25DALYY_02","O3CHGYY_01","GRLANYY_07",
-                  "GRLANYY_08","GRLANYY_09","GRLANYY_01","GRLANYY_04","GRLANYY_16","GRLANYY_12",
-                  "GRLANYY_17","GRLANYY_13","GRLANYY_18","GRLANYY_14","GRLANYY_10","GRLANYY_19",
-                  "GRLANYY_27","GRLANYY_26","GRLANYY_25","GRLANYY_23","GRLANYY_22","GRLANYY_21",
-                  "GRLANYY_07_2011","GRLANYY_07_2013","GRLANYY_08_2011","GRLANYY_08_2013","GRLANYY_09_2011",
-                  "GRLANYY_09_2013","GRLANYY_01_2011","GRLANYY_01_2013","GRLANYY_04_2011","GRLANYY_04_2013",
-                  "GRLANYY_16_2011","GRLANYY_16_2013","GRLANYY_12_2011","GRLANYY_12_2013","GRLANYY_17_2011",
-                  "GRLANYY_17_2013","GRLANYY_13_2011","GRLANYY_13_2013","GRLANYY_18_2011","GRLANYY_18_2013",
-                  "GRLANYY_14_2011","GRLANYY_14_2013","GRLANYY_10_2011","GRLANYY_10_2013","GRLANYY_19_2011",
-                  "GRLANYY_19_2013","GRLANYY_27_2011","GRLANYY_27_2013","GRLANYY_26_2011","GRLANYY_26_2013",
-                  "GRLANYY_25_2011","GRLANYY_25_2013","GRLANYY_23_2011","GRLANYY_23_2013","GRLANYY_22_2011",
-                  "GRLANYY_22_2013","GRLANYY_21_2011","GRLANYY_21_2013","MSDYY_MFS","MSDYY_SFS","MSDYY_MPPR",
-                  "MSDYY_DAPOP","MSDYY_SPPR","LGTNLTYY_01","WTHNRCYY_08","WTHNRCYY_13","ALE16_02","ALE16_03",
-                  "ALE16_04","ALE16_05","ALE16_06","ALE16_07","ALE16_08","ALE16_09","ALE16_10","ALE16_11",
-                  "ALE16_12","ALE16_13","bp_1yr_cold","bp_1yr","bp_1yr_warm","bp_5yrs_cold","bp_5yrs",
-                  "bp_5yrs_warm","bp_7days_cold","bp_7days_warm","bp_exp","Locrds_length_200m",
-                  "no2_1yr_cold","no2_1yr","no2_1yr_warm","no2_5yrs_cold","no2_5yrs","no2_5yrs_warm",
-                  "no2_7days_cold","no2_7days_warm","no2_exp","NO2F","o3_1yr_cold","o3_1yr","o3_1yr_warm",
-                  "o3_5yrs_cold","o3_5yrs","o3_5yrs_warm","o3_7days_cold","o3_7days_warm","o3_exp",
-                  "o38h_1yr_cold","o38h_1yr","o38h_1yr_warm","o38h_5yrs_cold","o38h_5yrs","o38h_5yrs_warm",
-                  "o38h_7days_cold","o38h_7days_warm","o38h_exp","PM25_08_12","pm25_1yr_cold","pm25_1yr",
-                  "pm25_1yr_warm","pm25_5yrs_cold","pm25_5yrs","pm25_5yrs_warm","pm25_7days_cold",
-                  "pm25_7days_warm","pm25_exp","rh_1yr_cold","rh_1yr","rh_1yr_warm","rh_5yrs_cold",
-                  "rh_5yrs","rh_5yrs_warm","rh_7days_cold","rh_7days_warm","rh_exp","so2_1yr_cold",
-                  "so2_1yr","so2_1yr_warm","so2_5yrs_cold","so2_5yrs","so2_5yrs_warm","so2_7days_cold",
-                  "so2_7days_warm","so2_exp","temp_1yr_cold","temp_1yr","temp_1yr_warm","temp_5yrs_cold",
-                  "temp_5yrs","temp_5yrs_warm","temp_7days_cold","temp_7days_warm","temp_exp", "DIA_MEDAGE_NB_COM", "Phwy_length_200m",
-                  "SLE_DRMDUR_NB_COM", "FAL_NMBR_NB_MCQ")
+numeric_vars <- c("HUP_FREE_MCQ", "GEN_DHDI",
+                  "COG_REYI_SCORE","COG_REYII_SCORE",
+                  "WLK_TIME_COM","GS_EXAM_AVG_COM",
+                  "AGE_NMBR","K10_DSCORE_MCQ","WHC_HIP_CM_COM","VA_ETDRS_BOTH_RSLT_COM")
 
 # Convert all other variables to factors except the pre-defined numeric variables
 CLSA.baseline.FU2 <- CLSA.baseline.FU2 %>%
@@ -81,16 +36,15 @@ CLSA.baseline.FU2 <- CLSA.baseline.FU2 %>%
   )) %>%
   dplyr::select(-c("X", "X.1", "entity_id","WGHTS_INFLATION"))
 
-
+# data
 CLSA.baseline.FU2<- CLSA.baseline.FU2 %>% dplyr::select(Binary_IADL.ADL_F2,HUP_FREE_MCQ,
-              GEN_DHDI,RET_RTRD,WLK_TIME_COM,
-              CR1_FRHC,PA2_SSPRTHR_MCQ,HGT_HEIGHT_M_COM,PER_DSCR_CON_MCQ,
+              GEN_DHDI,RET_RTRD,
+              CR1_FRHC,
               COG_REYI_SCORE,COG_REYII_SCORE,
-              WLK_TIME_COM,MSDYY_MFS,GS_EXAM_AVG_COM,ADL_DCLS,PA2_WALK_MCQ,
+              WLK_TIME_COM,GS_EXAM_AVG_COM,ADL_DCLS,PA2_WALK_MCQ,
               AGE_NMBR,K10_DSCORE_MCQ,WHC_HIP_CM_COM,
               CCT_F2, TRA_DSTATUS_MCQ.binary,
-              FAL_NMBR_NB_MCQ,
-              SEX_ASK, PA2_WALK_MCQ, PA2_SSPRTHR_MCQ, VA_ETDRS_BOTH_RSLT_COM,  MED_USE_MCQ, CCC_URIINC)%>%
+              SEX_ASK, VA_ETDRS_BOTH_RSLT_COM, CCC_URIINC)%>%
   drop_na(Binary_IADL.ADL_F2) 
 
 
@@ -153,30 +107,27 @@ nnet_param <- nnet_spec %>%
 
 ### recipes and grid search ####
 step.rec <- recipe(Binary_IADL.ADL_F2~HUP_FREE_MCQ+
-                   GEN_DHDI+RET_RTRD+WLK_TIME_COM+
-                   CR1_FRHC+PA2_SSPRTHR_MCQ+HGT_HEIGHT_M_COM+PER_DSCR_CON_MCQ+
+                   GEN_DHDI+RET_RTRD+
+                   CR1_FRHC+
                    COG_REYI_SCORE+COG_REYII_SCORE+
-                   WLK_TIME_COM+MSDYY_MFS+GS_EXAM_AVG_COM+ADL_DCLS+PA2_WALK_MCQ+
+                   WLK_TIME_COM+GS_EXAM_AVG_COM+ADL_DCLS+
                    AGE_NMBR+K10_DSCORE_MCQ+WHC_HIP_CM_COM+
-                  CCT_F2+ TRA_DSTATUS_MCQ.binary+
-                   FAL_NMBR_NB_MCQ+
-                   SEX_ASK+ PA2_WALK_MCQ+ PA2_SSPRTHR_MCQ+ VA_ETDRS_BOTH_RSLT_COM+  MED_USE_MCQ+ CCC_URIINC,
+                   CCT_F2+ TRA_DSTATUS_MCQ.binary+
+                   SEX_ASK+ PA2_WALK_MCQ+ VA_ETDRS_BOTH_RSLT_COM+ CCC_URIINC,
                   data = CLSA_train) %>%
   step_impute_bag(all_predictors(), seed_val = 123) %>%
-  #step_impute_bag(all_outcomes(), seed_val = 123) %>%
   step_dummy(all_nominal_predictors(), one_hot = TRUE)%>% 
   step_normalize(all_predictors())
 
 # recipe without dummy codes
 rec <- recipe(Binary_IADL.ADL_F2~HUP_FREE_MCQ+
-                GEN_DHDI+RET_RTRD+WLK_TIME_COM+
-                CR1_FRHC+PA2_SSPRTHR_MCQ+HGT_HEIGHT_M_COM+PER_DSCR_CON_MCQ+
+                GEN_DHDI+RET_RTRD+
+                CR1_FRHC+
                 COG_REYI_SCORE+COG_REYII_SCORE+
-                WLK_TIME_COM+MSDYY_MFS+GS_EXAM_AVG_COM+ADL_DCLS+PA2_WALK_MCQ+
+                WLK_TIME_COM+GS_EXAM_AVG_COM+ADL_DCLS+
                 AGE_NMBR+K10_DSCORE_MCQ+WHC_HIP_CM_COM+
                 CCT_F2+ TRA_DSTATUS_MCQ.binary+
-                FAL_NMBR_NB_MCQ+
-                SEX_ASK+ PA2_WALK_MCQ+ PA2_SSPRTHR_MCQ + VA_ETDRS_BOTH_RSLT_COM+  MED_USE_MCQ+ CCC_URIINC,
+                SEX_ASK+ PA2_WALK_MCQ+ VA_ETDRS_BOTH_RSLT_COM+ CCC_URIINC,
               data = CLSA_train) %>%
   step_impute_bag(all_predictors(), seed_val = 123) %>%
   step_impute_bag(all_outcomes(), seed_val = 123) 
@@ -190,9 +141,7 @@ workflow <- workflow_set(
 # xgboost workflow
 xgboost.wkflow <- workflow_set(
   preproc = list(step.rec),
-  models = list(LR = logistic, XG = xgboost, LR = logistic, NN = nnet_spec, KNN = knn)) 
-
-#XG = xgboost, LR = logistic, NN = nnet_spec, KNN = knn
+  models = list(LR = logistic, XG = xgboost, NN = nnet_spec, KNN = knn)) 
 
 # combine workflow
 all_workflows <- bind_rows(workflow , xgboost.wkflow) 
@@ -215,7 +164,41 @@ best_models <- grid_results %>%
 best_models$best_result
 best_models$wflow_id
 
-LR.best.pred<-best_models %>% filter(wflow_id == "recipe_LR") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model17")  
+LR.best.pred<-best_models %>% filter(wflow_id == "recipe_LR") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model03")  
+RF.best.pred<-best_models %>% filter(wflow_id == "simple_RF") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model09")  
+NB.best.pred<-best_models %>% filter(wflow_id == "simple_NB") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model01")  
+XG.best.pred<-best_models %>% filter(wflow_id == "recipe_XG") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model12")  
+NN.best.pred<-best_models %>% filter(wflow_id == "recipe_NN") %>% collect_predictions() %>% filter(.config == "Preprocessor1_Model03")  
+
+
+RF.best.pred <- RF.best.pred %>% mutate(model = "Random Forest")
+KNN.best.pred <- KNN.best.pred %>% mutate(model = "KNN")
+NB.best.pred <- NB.best.pred %>% mutate(model = "Naive Bayes")
+XG.best.pred <- XG.best.pred %>% mutate(model = "XGBoost")
+LR.best.pred <- LR.best.pred %>% mutate(model = "Logistic Regression")
+NN.best.pred<- NN.best.pred %>% mutate(model = "Neural Network")
+
+# Combine all predictions into a single dataframe
+all_predictions <- bind_rows(RF.best.pred, NB.best.pred, XG.best.pred, LR.best.pred, NN.best.pred)
+
+all_predictions %>% group_by(model)
+
+# Calculate ROC curve data for each model
+roc_data <- all_predictions %>%
+  group_by(model) %>%
+  roc_curve(truth = "Binary_IADL.ADL_F2", .pred_1, event_level = "second") %>%
+  ungroup() %>%
+  mutate(model = factor(model, levels = unique(model)))%>%
+  filter(!is.infinite(.threshold) & !is.na(.threshold))
+
+# Plot ROC curves
+ggplot(roc_data, aes(x = 1 - specificity, y = sensitivity, color = model)) +
+  geom_line() +
+  geom_abline(linetype = "dashed") +
+  labs(x = "1 - Specificity (False Positive Rate)", y = "Sensitivity (True Positive Rate)", color = "Model") +
+  ggtitle("ROC Curves for Binary IADL at Follow-up 2") +
+  theme_minimal() 
+
 
 ### extract performance metrics ####
 y<-LR.best.pred%>%
@@ -225,7 +208,6 @@ conf_mat(y, truth = Binary_IADL.ADL_F2, estimate = .pred_class)
 accuracy(y, truth = Binary_IADL.ADL_F2, estimate = .pred_class)
 sens(y, truth = Binary_IADL.ADL_F2, estimate = .pred_class)
 specificity(y, truth = Binary_IADL.ADL_F2, estimate = .pred_class)
-f_meas(y, truth = Binary_IADL.ADL_F2, estimate = .pred_class)
 
 # Extract metrics with optimal thresholds
 predictions <-LR.best.pred
@@ -252,7 +234,8 @@ optimal_threshold <- roc_data_filtered %>%
 print(optimal_threshold)
 
 # XG sensitivty
-optimal_threshold<- 1-0.02800519
+optimal_threshold<- 1-optimal_threshold
+optimal_threshold
 
 predictions_with_threshold <- predictions %>%
   mutate(predicted_class = if_else(.pred_1 >= optimal_threshold, 1, 0),
@@ -283,7 +266,7 @@ lr_workflow <- workflow() %>%
   add_recipe(step.rec)
 # fit the final model to the training data
 final_fit <- lr_workflow %>%
-  fit(data = CLSA_train)
+  fit(data = CLSA_test)
 # extract model fit
 fitted_model <- pull_workflow_fit(final_fit)
 # remove target variable from test data (so below code works)
@@ -295,7 +278,7 @@ test_prepared <- bake(trained_rec, new_data = CLSA_test.mod)
 # predictions <- predict(final_fit, newx = test_matrix, type = "class")
 predictions <- predict(fitted_model, new_data = test_prepared, type = "prob")
 # predict classes
-predicted_classes <- ifelse(predictions$.pred_0 > 1-0.9713455, "0", "1")
+predicted_classes <- ifelse(predictions$.pred_0 > 1-0.8620799, "0", "1")
 # actual_outcomes
 actual_outcomes <- as.factor(CLSA_test$Binary_IADL.ADL_F2)
 # Convert outcomes and predictions to factors explicitly
@@ -319,78 +302,14 @@ print(coefficients_tidy)
 coefficients_tidy <- coefficients_tidy %>%
   mutate(odds_ratio = exp(estimate))
 # View the coefficients with odds ratios
-print(coefficients_tidy, n=54)
-
-# model summary on full dataset ####
-# update recipe to use full dataset
-step.rec <- recipe(Binary_IADL.ADL_F2~HUP_FREE_MCQ+
-                     GEN_DHDI+RET_RTRD+WLK_TIME_COM+
-                     CR1_FRHC+PA2_SSPRTHR_MCQ+HGT_HEIGHT_M_COM+PER_DSCR_CON_MCQ+
-                     COG_REYI_SCORE+COG_REYII_SCORE+
-                     WLK_TIME_COM+MSDYY_MFS+GS_EXAM_AVG_COM+ADL_DCLS+PA2_WALK_MCQ+
-                     AGE_NMBR+K10_DSCORE_MCQ+WHC_HIP_CM_COM+
-                     CCT_F2+ TRA_DSTATUS_MCQ.binary+
-                     FAL_NMBR_NB_MCQ+
-                     SEX_ASK+ PA2_WALK_MCQ+ PA2_SSPRTHR_MCQ+ VA_ETDRS_BOTH_RSLT_COM+  MED_USE_MCQ+ CCC_URIINC,
-                   data = CLSA.baseline.FU2) %>%
-  step_impute_bag(all_predictors(), seed_val = 123) %>%
-  #step_impute_bag(all_outcomes(), seed_val = 123) %>%
-  step_dummy(all_nominal_predictors(), one_hot = TRUE)%>% 
-  step_normalize(all_predictors())
-# update lr_workflow
-lr_workflow <- workflow() %>%
-  add_model(glmnet_spec) %>%
-  add_recipe(step.rec)
-# fit the final model to the training data
-final_fit <- lr_workflow %>%
-  fit(data = CLSA.baseline.FU2)
-# extract model fit
-fitted_model <- pull_workflow_fit(final_fit)
-# Assuming `fitted_model` is your glmnet model object from a tidymodels workflow
-coefficients_tidy <- tidy(fitted_model, exponentiate = FALSE)
-# View the coefficients
-print(coefficients_tidy)
-# Add a column for Odds Ratios by exponentiating the estimates
-coefficients_tidy <- coefficients_tidy %>%
-  mutate(odds_ratio = exp(estimate))
-# View the coefficients with odds ratios
-print(coefficients_tidy, n=54)
-
-# confidence intervals for odds ratios ####
+print(coefficients_tidy, n=38)
 
 
-# Assuming `X` and `y` are your predictors and response matrix from the full dataset
-X <- model.matrix(Binary_IADL.ADL_F2 ~ ., data = CLSA.baseline.FU2)[, -1]  # remove intercept
-y <- full_dataset$Binary_IADL.ADL_F2
-
-# Define the function to fit the model
-bootstrap_glmnet <- function(data, indices) {
-  data_boot <- data[indices,]  # Resample the data
-  fit <- glmnet(x = X[indices, ], y = y[indices], family = "binomial")
-  coef(fit, s = "lambda.min")  # Extract coefficients at lambda.min
-}
-
-# Set up your full dataset (or however you've structured your inputs)
-full_data <- data.frame(X, y = as.numeric(y) - 1)  # Convert to binary numeric
-
-# Perform the bootstrap
-set.seed(123)  # for reproducibility
-boot_results <- boot(data = full_data, statistic = bootstrap_glmnet, R = 1000)  # R is the number of bootstrap replicates
-
-# Calculate confidence intervals from the bootstrap results
-boot_ci <- boot.ci(boot_results, type = "bca")  # using bias-corrected and accelerated (BCa) interval
-
-print(boot_ci)
-
-# export table to csv
 
 # descriptive table (full sample)####
 
-# note PA2_SSPRTHR_MCQ included twice in model; oops; also skip logic Q; need to omit?
-# walk time WLK_TIME_COM included twice, fuck...so is PA2_WALK_MCQ
-
 continuous_vars<-c("COG_REYI_SCORE","COG_REYII_SCORE","WHC_HIP_CM_COM",
-"FAL_NMBR_NB_MCQ", "AGE_NMBR","K10_DSCORE_MCQ", "WLK_TIME_COM", "MSDYY_MFS","GS_EXAM_AVG_COM", "HGT_HEIGHT_M_COM","PER_DSCR_CON_MCQ",
+"AGE_NMBR","K10_DSCORE_MCQ", "WLK_TIME_COM", "MSDYY_MFS","GS_EXAM_AVG_COM", "HGT_HEIGHT_M_COM",
 "VA_ETDRS_BOTH_RSLT_COM")
 
 results <- CLSA.baseline.FU2 %>%
@@ -435,19 +354,11 @@ CLSA.baseline.FU2 %>% group_by(CR1_FRHC) %>% summarise(count = n()) %>%
 # strenuous sports
 CLSA.baseline.FU2 %>% group_by(PA2_SSPRTHR_MCQ) %>% summarise(count = n()) %>%
   mutate(percent = count / sum(count) * 100)
-# medication use
-CLSA.baseline.FU2 %>% group_by(MED_USE_MCQ) %>% summarise(count = n()) %>%
-  mutate(percent = count / sum(count) * 100)
 
-# descriptive table (high ADL)####
-
-# note PA2_SSPRTHR_MCQ included twice in model; oops; also skip logic Q; need to omit?
-# walk time WLK_TIME_COM included twice, fuck...so is PA2_WALK_MCQ
-
+# descriptive table (functionally dependent)####
 CLSA.baseline.FU2.HighADL<-CLSA.baseline.FU2 %>% filter(Binary_IADL.ADL_F2==0)
-
-continuous_vars<-c("COG_REYI_SCORE","COG_REYII_SCORE","WHC_HIP_CM_COM",
-                   "FAL_NMBR_NB_MCQ", "AGE_NMBR","K10_DSCORE_MCQ", "WLK_TIME_COM", "MSDYY_MFS","GS_EXAM_AVG_COM", "HGT_HEIGHT_M_COM","PER_DSCR_CON_MCQ",
+str(CLSA.baseline.FU2.HighADL)
+continuous_vars<-c("COG_REYI_SCORE","COG_REYII_SCORE","WHC_HIP_CM_COM", "AGE_NMBR","K10_DSCORE_MCQ", "WLK_TIME_COM", "GS_EXAM_AVG_COM", 
                    "VA_ETDRS_BOTH_RSLT_COM")
 
 results <- CLSA.baseline.FU2.HighADL %>%
@@ -489,23 +400,12 @@ CLSA.baseline.FU2.HighADL %>% group_by(RET_RTRD) %>% summarise(count = n()) %>%
 # formal care
 CLSA.baseline.FU2.HighADL %>% group_by(CR1_FRHC) %>% summarise(count = n()) %>%
   mutate(percent = count / sum(count) * 100)
-# strenuous sports
-CLSA.baseline.FU2.HighADL %>% group_by(PA2_SSPRTHR_MCQ) %>% summarise(count = n()) %>%
-  mutate(percent = count / sum(count) * 100)
-# medication use
-CLSA.baseline.FU2.HighADL %>% group_by(MED_USE_MCQ) %>% summarise(count = n()) %>%
-  mutate(percent = count / sum(count) * 100)
 
-# descriptive table (high ADL)####
 
-# note PA2_SSPRTHR_MCQ included twice in model; oops; also skip logic Q; need to omit?
-# walk time WLK_TIME_COM included twice, fuck...so is PA2_WALK_MCQ
+# descriptive table (functionally independent)####
 
 CLSA.baseline.FU2.LowADL<-CLSA.baseline.FU2 %>% filter(Binary_IADL.ADL_F2==1)
-
-continuous_vars<-c("COG_REYI_SCORE","COG_REYII_SCORE","WHC_HIP_CM_COM",
-                   "FAL_NMBR_NB_MCQ", "AGE_NMBR","K10_DSCORE_MCQ", "WLK_TIME_COM", "MSDYY_MFS","GS_EXAM_AVG_COM", "HGT_HEIGHT_M_COM","PER_DSCR_CON_MCQ",
-                   "VA_ETDRS_BOTH_RSLT_COM")
+str(CLSA.baseline.FU2.LowADL)
 
 results <- CLSA.baseline.FU2.LowADL %>%
   dplyr::select(all_of(continuous_vars)) %>%
